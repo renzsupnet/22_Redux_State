f124ac6f47db50de068db4b208be2be0188162e6
Renz Carl Supnet <31431868+renzsupnet@users.noreply.github.com>
2024-09-19 23:15:21 -0700

    Added store.js that imports createStore, and reducers.

--- client/src/utils/GlobalState.jsx
+++ client/src/utils/GlobalState.jsx
@@ -1,8 +1,9 @@
 import { createContext, useContext, useReducer } from "react";
 import { reducer } from './reducers'
+import { Provider } from 'react-redux';
 
 const StoreContext = createContext();
-const { Provider } = StoreContext;
+// const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useReducer(reducer, {
